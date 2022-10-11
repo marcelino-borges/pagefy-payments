99438a3fca210ae3b8357e4516582415fb1e6c78
Marcelino Borges <botelho_gt@hotmail.com>
2022-10-11 18:42:40 +0100

    cleanup and unformization with the api

--- src/server.ts
+++ src/server.ts
@@ -6,36 +6,67 @@ import dotenvSafe from "dotenv-safe";
 import swaggerFile from "../swagger_output.json";
 import swaggerUi from "swagger-ui-express";
 import connectMongo from "./config/mongo";
-import admin from "firebase-admin";
-import firebaseConfig from "./config/firebase";
 import { initializeStripe } from "./config/stripe";
-
-admin.initializeApp({
-  credential: admin.credential.cert(JSON.parse(firebaseConfig)),
-  storageBucket: JSON.parse(firebaseConfig).storageBucket,
-});
+import log from "./utils/logs";
 
 dotenvSafe.config({
   allowEmptyValues: true,
 });
 
-const PORT = parseInt(process.env.PORT as string, 10);
+const canReadEnv = String(process.env.MONGO_CONNECTION_STRING).includes(
+  "mongodb+srv://"
+);
 
-console.log("PORT on env: ", PORT);
+if (canReadEnv) {
+  log.success(".ENV verified!");
+  const PORT = parseInt(process.env.PORT as string, 10);
 
-initializeStripe();
+  console.log("PORT on env: ", PORT);
 
-const app = express();
-app.use(cors());
+  initializeStripe();
 
-connectMongo();
+  const app = express();
 
-app.use(helmet());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use("/api/v1", routes);
+  const publicCors = cors();
+  // const privateCors = cors({
+  //   origin: (origin, callback) => {
+  //     if (!origin || ALLOWED_ORIGINS.indexOf(origin) === -1) {
+  //       log.info("Blocked access from origin: " + origin);
+  //       var msg =
+  //         "The CORS policy for this site does not " +
+  //         "allow access from the specified Origin.";
+  //       return callback(new Error(msg), false);
+  //     }
+  //     return callback(null, true);
+  //   },
+  // });
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+  connectMongo()
+    .then(() => {
+      app.use(publicCors);
+      app.use(helmet());
+      app.use(express.urlencoded({ extended: false }));
+      app.use(express.json());
+      app.options("*", publicCors);
+      app.use("/health-check", publicCors, (_, res) =>
+        res.status(200).json({ message: "API running." })
+      );
+      app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+      app.use("/api/v1", routes);
+
+      const server = app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`);
+      });
+
+      server.timeout = 3000;
+    })
+    .catch((e) =>
+      log.error(
+        "Error trying to connect to MongoDB. API not running.",
+        "Details:",
+        e
+      )
+    );
+} else {
+  log.error(".ENV not available. API not running.");
+}
