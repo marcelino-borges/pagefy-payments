7a2a576787aa74acfb314004b90093bb9e2c64dd
Marcelino Borges <botelho_gt@hotmail.com>
2022-10-14 23:12:58 +0100

    server test log

--- src/services/stripe.service.ts
+++ src/services/stripe.service.ts
@@ -233,7 +233,7 @@ export const hookPaymentFromStripe = async (req: Request, res: Response) => {
     }
   );
 
-  log.info(`PaymentIntent: `, paymentIntent);
+  log.info(`PaymentIntent: `, JSON.stringify(paymentIntent));
   if (updatedSubscription) {
     log.warn(`22222222222`);
     const userFound = await UserDb.findOne({
